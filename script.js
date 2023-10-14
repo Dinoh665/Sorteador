71c460b80700cb86cea5688d990c618f15647edb
Dinoh665 <fruhaufandrei@gmail.com>
2023-10-14 13:01:54 -0300

    versão final

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 
 const button = document.querySelector("button")
-
+const resultado = document.querySelector(".resultado")
 
 function generateNumber() {
     const min = Math.ceil(document.querySelector(".in1").value)
-    const max = Math.floor(document.querySelector(".in2").value)    
+    const max = Math.floor(document.querySelector(".in2").value)
+    
 
     const result = Math.floor(Math.random() * (max - min  + 1)) + min;
 
     console.log(min)
     console.log(max)
 
-    alert(result)
+    if(min >= max){
+        resultado.innerHTML = "O primeiro número deve ser MENOR que o segundo"
+    }else{
+        resultado.innerHTML = result
+    }
+
+    
+
 }
 
 button.addEventListener("click", generateNumber)
